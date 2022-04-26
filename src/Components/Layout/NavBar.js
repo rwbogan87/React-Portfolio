621cde8e9cf26e3369331f7746b11ba568139d2b
ryan <user@superbook.com>
2022-04-26 14:35:35 -0400

    redesigning nav buttons and active tab

--- src/Components/Layout/NavBar.js
+++ src/Components/Layout/NavBar.js
@@ -1,15 +1,23 @@
 import '../../Styling/NavBar.css'
 import { Link } from 'react-router-dom'
+import { useState } from 'react';
 // import { BreadcrumbItem } from 'reactstrap'
 //* not crazy about the divider
-
 function NavBar({routes}) {
+const [focusBorder, setFocusBorder] = useState(null);
+
+let activeNavFunction = (data) => {
+  console.log(data)
+  setFocusBorder(data)
+}
+
     return (
       <div className="NavBar">
         <nav className="routes-container">
           {routes.map((route) => {
             return (
-              <Link tag={Link} to={route} key={route} className="routes-link">{route}</Link>
+              <Link tag={Link} to={route} key={route} className="routes-link" id={focusBorder}
+              onClick={() => activeNavFunction(route)}>{route}</Link>
             )
           })}
         </nav>
